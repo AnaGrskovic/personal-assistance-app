e32d556097de0c3f68044f89b1932d272c54d31f
AnaGrskovic <ana.grskovic56@gmail.com>
2023-10-16 15:02:26 +0200

    Initialize Spring project

---
+++ src/main/java/insa/project/personalassistanceapp/PersonalAssistanceAppApplication.java
@@ -0,0 +1,13 @@
+package insa.project.personalassistanceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalAssistanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalAssistanceAppApplication.class, args);
+	}
+
+}
