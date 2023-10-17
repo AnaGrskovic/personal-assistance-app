515321826ebec2bcdde68ebddc45428fe28d9d95
AnaGrskovic <ana.grskovic56@gmail.com>
2023-10-17 17:01:12 +0200

    Modify person in need class

--- src/main/java/insa/project/personalassistanceapp/model/PersonInNeed.java
+++ src/main/java/insa/project/personalassistanceapp/model/PersonInNeed.java
@@ -1,6 +1,6 @@
 package insa.project.personalassistanceapp.model;
 
-import insa.project.personalassistanceapp.util.Role;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,8 +9,11 @@ import java.util.Date;
 @Getter
 @Setter
 @NoArgsConstructor
-public class PersonInNeed extends User {
+@AllArgsConstructor
+public class PersonInNeed {
 
+    private User user;
+    private Long personInNeedId;
     private String firstName;
     private String lastName;
     private Date dateOfBirth;
@@ -19,16 +22,4 @@ public class PersonInNeed extends User {
     private String postalCode;
     private String country;
 
-    public PersonInNeed(String username, String password, String firstName, String lastName, Date dateOfBirth, String address, String city, String postalCode, String country) {
-        this.role = Role.PERSON_IN_NEED;
-        this.username = username;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.dateOfBirth = dateOfBirth;
-        this.address = address;
-        this.city = city;
-        this.postalCode = postalCode;
-        this.country = country;
-    }
 }
--- src/main/java/insa/project/personalassistanceapp/model/User.java
+++ src/main/java/insa/project/personalassistanceapp/model/User.java
@@ -1,10 +1,18 @@
 package insa.project.personalassistanceapp.model;
 
 import insa.project.personalassistanceapp.util.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
-    protected Long id;
+    protected Long userId;
     protected Role role;
     protected String username;
     protected String password;
