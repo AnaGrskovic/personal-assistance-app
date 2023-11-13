2dd52b8030fae8a1a7856055619c710d3473b8c2
AnaGrskovic <ana.grskovic56@gmail.com>
2023-11-13 10:15:32 +0100

    Fix mistakes in sql

--- src/main/resources/db.migration/V1__initialize.sql
+++ src/main/resources/db.migration/V1__initialize.sql
@@ -1,32 +1,31 @@
--- User Table
-CREATE TABLE user_table (
-                            user_id SERIAL PRIMARY KEY,
-                            role VARCHAR(255) NOT NULL,
-                            username VARCHAR(255) NOT NULL,
-                            password VARCHAR(255) NOT NULL
+CREATE TABLE user (
+  user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  role VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
--- PersonInNeed Table
-CREATE TABLE person_in_need_table (
-                                      person_in_need_id SERIAL PRIMARY KEY,
-                                      user_id BIGINT REFERENCES user_table(user_id),
-                                      first_name VARCHAR(255) NOT NULL,
-                                      last_name VARCHAR(255) NOT NULL,
-                                      date_of_birth DATE,
-                                      address VARCHAR(255),
-                                      city VARCHAR(255),
-                                      postal_code VARCHAR(20),
-                                      country VARCHAR(255)
+CREATE TABLE person_in_need (
+    person_in_need_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    date_of_birth DATE,
+    address VARCHAR(255),
+    city VARCHAR(255),
+    postal_code VARCHAR(20),
+    country VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
--- ProfessionalInCharge Table
-CREATE TABLE professional_in_charge_table (
-    user_id BIGINT REFERENCES user_table(user_id) PRIMARY KEY
-    -- Add additional columns as needed for ProfessionalInCharge
+CREATE TABLE professional_in_charge (
+    professional_in_charge_id BIGINT PRIMARY KEY,
+    user_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
--- Volunteer Table
-CREATE TABLE volunteer_table (
-    user_id BIGINT REFERENCES user_table(user_id) PRIMARY KEY
-    -- Add additional columns as needed for Volunteer
+CREATE TABLE volunteer (
+   volunteer_id BIGINT PRIMARY KEY,
+   user_id BIGINT,
+   FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
