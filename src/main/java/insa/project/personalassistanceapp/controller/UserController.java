e2c02dd9ed1c5483f4f99ce3c65960bc7899e293
AnaGrskovic <ana.grskovic56@gmail.com>
2023-11-13 12:10:46 +0100

    Add html for person in need registration

--- src/main/java/insa/project/personalassistanceapp/controller/UserController.java
+++ src/main/java/insa/project/personalassistanceapp/controller/UserController.java
@@ -3,8 +3,11 @@ package insa.project.personalassistanceapp.controller;
 import insa.project.personalassistanceapp.model.dto.*;
 import insa.project.personalassistanceapp.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
 @RestController("/user")
 @RequiredArgsConstructor
@@ -12,9 +15,23 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/person-in-need/registration")
+    /*@PostMapping("/person-in-need/registration")
     public PersonInNeedDto personInNeedRegistration (PersonInNeedForm personInNeedForm){
         return userService.personInNeedRegistration(personInNeedForm);
+    }*/
+
+    @GetMapping("/person-in-need/registration/show")
+    public String showPersonInNeedRegistrationForm(Model model) {
+        PersonInNeedForm personInNeedForm = new PersonInNeedForm();
+        model.addAttribute("personInNeedForm", personInNeedForm);
+        return "person-in-need-registration";
+    }
+
+    @PostMapping("person-in-need/registration/show")
+    public String processPersonInNeedRegistrationForm(@ModelAttribute("personInNeedForm") PersonInNeedForm personInNeedForm) {
+        userService.personInNeedRegistration(personInNeedForm);
+        // Redirect to a success page or home page
+        return "successfully-registered";
     }
 
     @PostMapping("/volunteer/registration")
